3129b2b75f94184c21f43d99be1709cef810ff5f
Strthckr <48113218+Strthckr@users.noreply.github.com>
2025-08-06 13:59:04 +0530

    Small change in route

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ from dotenv import load_dotenv
 import os
 import bcrypt
 import traceback
+import re
+
 
 # ─── LOAD ENVIRONMENT VARIABLES ────────────────────────────────────────────────
 # Load variables from a .env file (good for DB credentials, secret keys, etc.)
